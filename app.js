b42882f628092d8eadf782c3d8002d10fd96fbc3
tung.vlx <bivabin100@gmail.com>
2022-04-21 09:28:41 +0700

    Update

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 document.addEventListener('click', function(e) {
   console.log(e.target);
 })
+
+var welcomeBtn = document.querySelector('.welcome button');
+var alert=document.querySelector('.alert_noti');
+welcomeBtn.addEventListener('click', function() {
+  alert.classList.toggle('hide');
+})

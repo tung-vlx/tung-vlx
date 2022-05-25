a48903557806de1564944dfc084932e43e5a1192
Tung <bivabin100@gmail.com>
2022-05-25 13:24:10 +0700

    update personal information + navigation bar

---
+++ app.js
@@ -0,0 +1,5 @@
+var btnToHomePage = document.querySelector("#btnToHomePage");
+btnToHomePage.addEventListener('click', function() {
+  console.log("dc roi di thoi");
+  window.location="index.html";
+})

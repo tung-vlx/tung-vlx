f4ea08c589ce06693ae170da5d36ff80f8893859
Tung <bivabin100@gmail.com>
2022-05-26 21:00:17 +0700

    Download function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var btnToHomePage = document.querySelector("#btnToHomePage");
 btnToHomePage.addEventListener('click', function() {
-  console.log("dc roi di thoi");
   window.location="index.html";
+})
+var downloadChannelDevelopmentAssistant = document.querySelector("#downloadChannelDevelopmentAssistant");
+downloadChannelDevelopmentAssistant.addEventListener('click', function() {
+  window.location="resources/[Channel Development Assistant] Võ Lê Xuân Tùng.pdf";
 })
